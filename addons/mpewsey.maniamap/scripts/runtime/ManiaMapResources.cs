fa28038bf2636967d0af2f0a935adb25cdac8a45
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2024-05-09 14:11:13 -0400

    Update graph edge

--- addons/mpewsey.maniamap/scripts/runtime/ManiaMapResources.cs
+++ addons/mpewsey.maniamap/scripts/runtime/ManiaMapResources.cs
@@ -2,7 +2,7 @@ namespace MPewsey.ManiaMapGodot
 {
     public static class ManiaMapResources
     {
-        public const string DoorCodeFlags = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+        public const string DoorCodeFlags = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,All:67108863";
         public const string LayoutGraphEditorScene = "uid://ckyjrhwvcs6fi";
     }
 }
--- addons/mpewsey.maniamap/scripts/runtime/graphs/LayoutGraphEdge.cs
+++ addons/mpewsey.maniamap/scripts/runtime/graphs/LayoutGraphEdge.cs
@@ -7,19 +7,39 @@ namespace MPewsey.ManiaMapGodot.Graphs
     [Tool]
     public partial class LayoutGraphEdge : Resource
     {
-        [Export] public int FromNode { get; set; }
-        [Export] public int ToNode { get; set; }
-        [Export] public string Name { get; set; }
-        [Export] public TemplateGroup TemplateGroup { get; set; }
-        [Export] public EdgeDirection EdgeDirection { get; set; }
-        [Export] public Color Color { get; set; }
-        [Export] public int Z { get; set; }
-        [Export] public bool RequireRoom { get; set; }
-        [Export] public float RoomChance { get; set; }
-        [Export] public string[] Tags { get; set; } = Array.Empty<string>();
+        private int _fromNode;
+        [Export] public int FromNode { get => _fromNode; set => SetField(ref _fromNode, value); }
 
+        private int _toNode;
+        [Export] public int ToNode { get => _toNode; set => SetField(ref _toNode, value); }
+
+        private string _name;
+        [Export] public string Name { get => _name; set => SetField(ref _name, value); }
+
+        private TemplateGroup _templateGroup;
+        [Export] public TemplateGroup TemplateGroup { get => _templateGroup; set => SetField(ref _templateGroup, value); }
+
+        private EdgeDirection _edgeDirection;
+        [Export] public EdgeDirection EdgeDirection { get => _edgeDirection; set => SetField(ref _edgeDirection, value); }
+
+        private Color _color;
+        [Export] public Color Color { get => _color; set => SetField(ref _color, value); }
+
+        private int _z;
+        [Export] public int Z { get => _z; set => SetField(ref _z, value); }
+
+        private bool _requireRoom;
+        [Export] public bool RequireRoom { get => _requireRoom; set => SetField(ref _requireRoom, value); }
+
+        private float _roomChance;
+        [Export] public float RoomChance { get => _roomChance; set => SetField(ref _roomChance, value); }
+
+        private string[] _tags = Array.Empty<string>();
+        [Export] public string[] Tags { get => _tags; set => SetField(ref _tags, value); }
+
+        private int _code;
         [ExportGroup("Code")]
-        [Export] public DoorCode Code { get; set; }
+        [Export(PropertyHint.Flags, ManiaMapResources.DoorCodeFlags)] public int Code { get => _code; set => SetField(ref _code, value); }
 
         private void SetField<T>(ref T field, T value)
         {
