3dc2510ef526f707501333b6316df07965a99d1e
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2024-05-20 18:02:10 -0400

    Add doc strings

--- addons/mpewsey.maniamap/scripts/runtime/TemplateGroupEntry.cs
+++ addons/mpewsey.maniamap/scripts/runtime/TemplateGroupEntry.cs
@@ -4,14 +4,32 @@ using System.Collections.Generic;
 
 namespace MPewsey.ManiaMapGodot
 {
+    /// <summary>
+    /// An TemplateGroup entry with a room template and its constraints.
+    /// </summary>
     [Tool]
     [GlobalClass]
     public partial class TemplateGroupEntry : Resource
     {
+        /// <summary>
+        /// The room template.
+        /// </summary>
         [Export] public RoomTemplateResource RoomTemplate { get; set; }
+
+        /// <summary>
+        /// The minimum quantity of rooms with this template to include in the layout.
+        /// </summary>
         [Export(PropertyHint.Range, "0,100,1,or_greater")] public int MinQuantity { get; set; }
+
+        /// <summary>
+        /// The maximum quantity of rooms with this template to include in the layout.
+        /// </summary>
         [Export(PropertyHint.Range, "0,100,1,or_greater")] public int MaxQuantity { get; set; } = int.MaxValue;
 
+        /// <summary>
+        /// Returns a new ManiaMap template groups entry for use by the procedural generator.
+        /// </summary>
+        /// <param name="templateCache">A dictionary of cached room templates by resource.</param>
         public TemplateGroupsEntry GetMMTemplateGroupsEntry(Dictionary<RoomTemplateResource, RoomTemplate> templateCache)
         {
             if (!templateCache.TryGetValue(RoomTemplate, out var template))
