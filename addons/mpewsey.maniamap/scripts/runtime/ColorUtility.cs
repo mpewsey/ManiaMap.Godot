1003e22c407fbbcd264b98ccc0350273a34d3265
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2024-05-20 11:15:24 -0400

    Add doc strings

--- addons/mpewsey.maniamap/scripts/runtime/ColorUtility.cs
+++ addons/mpewsey.maniamap/scripts/runtime/ColorUtility.cs
@@ -3,13 +3,24 @@ using MPewsey.ManiaMap;
 
 namespace MPewsey.ManiaMapGodot
 {
+    /// <summary>
+    /// Contains methods for converting colors.
+    /// </summary>
     public static class ColorUtility
     {
+        /// <summary>
+        /// Converts a Godot Color to a ManiaMap Color4.
+        /// </summary>
+        /// <param name="color">The Godot color.</param>
         public static Color4 ConvertColorToColor4(Color color)
         {
             return new Color4((byte)color.R8, (byte)color.G8, (byte)color.B8, (byte)color.A8);
         }
 
+        /// <summary>
+        /// Converts a ManiaMap Color4 to a Godot Color.
+        /// </summary>
+        /// <param name="color">The ManiaMap color.</param>
         public static Color ConvertColor4ToColor(Color4 color)
         {
             return Color.Color8(color.R, color.G, color.B, color.A);
--- addons/mpewsey.maniamap/scripts/runtime/DoorNode2D.cs
+++ addons/mpewsey.maniamap/scripts/runtime/DoorNode2D.cs
@@ -5,20 +5,44 @@ using System.Collections.Generic;
 
 namespace MPewsey.ManiaMapGodot
 {
+    /// <summary>
+    /// A possible door location connecting two RoomNode2D.
+    /// </summary>
     [Tool]
     [GlobalClass]
     [Icon(ManiaMapResources.Icons.DoorNode2DIcon)]
     public partial class DoorNode2D : CellChild2D
     {
+        /// <summary>
+        /// A dictionary of doors in the scene by room ID.
+        /// </summary>
         private static Dictionary<Uid, LinkedList<DoorNode2D>> ActiveRoomDoors { get; } = new Dictionary<Uid, LinkedList<DoorNode2D>>();
 
+        /// <summary>
+        /// If true, auto assigns the door direction based on its position when auto assign is run.
+        /// Disable this flag if you wish to control this value manually.
+        /// </summary>
         [Export] public bool AutoAssignDirection { get; set; } = true;
+
+        /// <summary>
+        /// The direction where the door leads relative to its containing cell.
+        /// </summary>
         [Export] public DoorDirection DoorDirection { get; set; }
+
+        /// <summary>
+        /// The door type. See the ManiaMap DoorType documentation for more information.
+        /// </summary>
         [Export] public DoorType DoorType { get; set; }
 
+        /// <summary>
+        /// The door code to which this door may connect. Door codes can connect if they intersect.
+        /// </summary>
         [ExportGroup("Door Code")]
         [Export(PropertyHint.Flags, ManiaMapResources.Enums.DoorCodeFlags)] public int DoorCode { get; set; }
 
+        /// <summary>
+        /// The door's door and room connection information.
+        /// </summary>
         public DoorConnection DoorConnection { get; private set; }
 
         public override void _Ready()
@@ -45,11 +69,15 @@ namespace MPewsey.ManiaMapGodot
                 RemoveFromActiveRoomDoors();
         }
 
+        /// <summary>
+        /// Returns the ManiaMap door used for procedural generation.
+        /// </summary>
         public Door GetMMDoor()
         {
             return new Door(DoorType, (DoorCode)DoorCode);
         }
 
+        /// <inheritdoc/>
         public override void AutoAssign(RoomNode2D room)
         {
             base.AutoAssign(room);
@@ -58,11 +86,18 @@ namespace MPewsey.ManiaMapGodot
                 DoorDirection = room.FindClosestDoorDirection(Row, Column, GlobalPosition);
         }
 
+        /// <summary>
+        /// Returns true if a connection exists within the `Layout` for the door.
+        /// </summary>
         public bool DoorExists()
         {
             return DoorConnection != null;
         }
 
+        /// <summary>
+        /// Returns the associated door connection within the `Layout`.
+        /// If the door does not have a connect, returns null.
+        /// </summary>
         private DoorConnection FindDoorConnection()
         {
             var roomId = Room.RoomLayout.Id;
@@ -77,6 +112,10 @@ namespace MPewsey.ManiaMapGodot
             return null;
         }
 
+        /// <summary>
+        /// Returns the ID of the room to which this door connects.
+        /// If this door does not exist, returns Uid(-1, -1, -1).
+        /// </summary>
         public Uid ToRoomId()
         {
             if (!DoorExists())
@@ -85,6 +124,9 @@ namespace MPewsey.ManiaMapGodot
             return DoorConnection.GetConnectingRoom(Room.RoomLayout.Id);
         }
 
+        /// <summary>
+        /// Adds the door to the active room doors dictionary.
+        /// </summary>
         private void AddToActiveRoomDoors()
         {
             if (Room.IsInitialized)
@@ -101,6 +143,9 @@ namespace MPewsey.ManiaMapGodot
             }
         }
 
+        /// <summary>
+        /// Removes the door from the active room doors dictionary.
+        /// </summary>
         private void RemoveFromActiveRoomDoors()
         {
             if (Room.IsInitialized)
@@ -117,6 +162,11 @@ namespace MPewsey.ManiaMapGodot
             }
         }
 
+        /// <summary>
+        /// Returns the door in the scene with the matching room ID and door connection.
+        /// </summary>
+        /// <param name="roomId">The door's room ID.</param>
+        /// <param name="doorConnection">The door's connection.</param>
         public static DoorNode2D FindActiveDoor(Uid roomId, DoorConnection doorConnection)
         {
             if (doorConnection != null && ActiveRoomDoors.TryGetValue(roomId, out var doors))
