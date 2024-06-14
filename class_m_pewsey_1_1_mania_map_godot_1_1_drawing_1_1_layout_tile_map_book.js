cfb122d317015fa5efc0c90d4ad36a02328b2cb2
mpewsey <mpewsey@users.noreply.github.com>
2024-06-14 19:18:15 +0000

    deploy: 87fdfb04baa48a2c1e9f56289ae444a77e66e5dc

--- class_m_pewsey_1_1_mania_map_godot_1_1_drawing_1_1_layout_tile_map_book.js
+++ class_m_pewsey_1_1_mania_map_godot_1_1_drawing_1_1_layout_tile_map_book.js
@@ -4,7 +4,7 @@ var class_m_pewsey_1_1_mania_map_godot_1_1_drawing_1_1_layout_tile_map_book =
     [ "DrawPages", "class_m_pewsey_1_1_mania_map_godot_1_1_drawing_1_1_layout_tile_map_book.html#ad637d698e9095adc13dc982e46dd33a4", null ],
     [ "GetPageLayerCoordinates", "class_m_pewsey_1_1_mania_map_godot_1_1_drawing_1_1_layout_tile_map_book.html#a76e368f4915487833840651fc8542ece", null ],
     [ "GetPages", "class_m_pewsey_1_1_mania_map_godot_1_1_drawing_1_1_layout_tile_map_book.html#ac78aac003d5a05836ae3c2f48a72e0e8", null ],
-    [ "SetOnionMapColors", "class_m_pewsey_1_1_mania_map_godot_1_1_drawing_1_1_layout_tile_map_book.html#a72e875761e586e1637878eaa1847a697", null ],
+    [ "SetOnionskinColors", "class_m_pewsey_1_1_mania_map_godot_1_1_drawing_1_1_layout_tile_map_book.html#a0ce0a143cf1de96f1fc9d0d5a9008efb", null ],
     [ "SizePages", "class_m_pewsey_1_1_mania_map_godot_1_1_drawing_1_1_layout_tile_map_book.html#af62b85d595632954ec6e857ab6bbe51e", null ],
     [ "PageLayerCoordinates", "class_m_pewsey_1_1_mania_map_godot_1_1_drawing_1_1_layout_tile_map_book.html#a982c0148a7907fa670ad183adeee3300", null ],
     [ "Pages", "class_m_pewsey_1_1_mania_map_godot_1_1_drawing_1_1_layout_tile_map_book.html#ad228a9e88d615d623a636a2d0d627924", null ]
